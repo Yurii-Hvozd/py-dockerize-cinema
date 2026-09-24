d1ba79c08c3b064a9d46475b7b03704bdfae11b3
Yurii <yura.gvozd.2002@gmail.com>
2026-09-24 22:34:45 +0300

    'Solution'

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -139,8 +139,10 @@ USE_TZ = False
 
 STATIC_URL = "static/"
 
-MEDIA_URL = "/files/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/files/media"
+MEDIA_URL = "/media/"
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
